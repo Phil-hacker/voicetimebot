75596a2cb38e48c00fc769972edf7c32970102e5
Philipp Meyer <phil.mey.06@gmail.com>
2023-11-06 00:10:05 +0100

    added exit command and connection cleanup

--- src/control_server.rs
+++ src/control_server.rs
@@ -24,25 +24,37 @@ pub fn create_control_server(port: u16, db: Arc<DbManager>, db_path: &str) {
         let mut last_save = Instant::now();
         let mut connections = Vec::new();
         let mut buffer = [0u8; 1 << 16];
+        let mut dead_connections = vec![];
         loop {
             if let Ok(connection) = receiver.try_recv() {
                 if let Ok(_) = connection.0.set_nonblocking(true) {
                     connections.push(connection.0);
                 }
             }
-            for connection in connections.iter_mut() {
+            for (i, connection) in connections.iter_mut().enumerate() {
                 if let Ok(size) = connection.read(&mut buffer) {
                     if size == 0 {
+                        dead_connections.push(i);
                         continue;
                     }
                     if let Ok(command) = std::str::from_utf8(&buffer[0..size]).map(|v| v.trim()) {
                         match command {
                             "save" => db.save_db(db_path.clone()),
                             "stop" => db.stop_and_save_db(db_path.clone()),
+                            "exit" => {
+                                connection
+                                    .shutdown(std::net::Shutdown::Both)
+                                    .unwrap_or_else(|err| eprintln!("{err}"));
+                                dead_connections.push(i)
+                            }
                             _ => {}
                         }
                     }
                 }
+            }
+            dead_connections.reverse();
+            for i in dead_connections.drain(..) {
+                connections.remove(i);
             }
             if last_save.elapsed().as_secs() > SAVE_INTERVALL {
                 last_save = Instant::now();
